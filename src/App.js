79e53350575dcfbc1c2a3142ee06247f4b455fa7
jhonludena <jhon_ludena_r@outlook.es>
2020-06-06 20:49:39 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Navigation from "./components/layout/Navigation"
+import Landing from "./components/layout/Landing"
+//import Register from "./components/auth/Register"
+//import Login from "./components/auth/Login"
+//import Dashboard from "./components/dashboard/Dashboard"
+
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Hello, Jhon Ludeña</h1>
-    </div>
+    <Router>
+      <Navigation />
+      <Container>
+        <Route exact path="/" component={Landing} />
+      </Container>
+    </Router>
   )
 }
 
